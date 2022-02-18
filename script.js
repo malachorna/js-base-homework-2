6e78c4eea1c441a574a893e1628f737888c8caab
Iryna Prytula <96309174+malachorna@users.noreply.github.com>
2022-02-18 13:26:14 +0200

    Iryna Prytula

---
+++ script.js
@@ -0,0 +1,25 @@
+let number = prompt("Enter your number", '')
+if (number % 2 === 0) {
+    alert("even")
+} else {
+    alert("odd")
+}
+
+
+let number = prompt("Enter days", '')
+if (number >= 7) {
+    alert(40 * number - 50)
+} else if (number >= 3 && < 7) {
+    alert(40 * number - 20)
+} else(number * 40) {
+    alert(40 ** number)
+}
+
+
+let year = prompt("Enter year")
+if (year < 0) {
+    alert("no year exist")
+} else(year > 0 && 2022) {
+    alert(...)
+}
+console.log(typeof parseint(()))
